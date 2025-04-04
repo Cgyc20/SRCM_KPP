485e5a4c1d83f1539d8662de87952a6887753b7c
Cgyc20 <cgyc20@bath.ac.uk>
2025-04-04 10:33:42 +0100

    Add tests for calculate_total_mass function and rename existing test

--- tests/test_c_functions.py
+++ tests/test_c_functions.py
@@ -41,7 +41,7 @@ def test_approximate_mass_left_hand_3(wrapper):
     
     assert result[0] == 0.4 #This should be 1*0.1+1*0.1+1*0.1+1*0.1
 
-def test_approximate_mass_left_hand_3(wrapper):
+def test_approximate_mass_left_hand_4(wrapper):
     """This will test the approximate left hand of a PDE discretised list"""
     SSA_M = 10
     PDE_multiple = 10
@@ -51,3 +51,37 @@ def test_approximate_mass_left_hand_3(wrapper):
     result = wrapper.approximate_mass_left_hand(SSA_M, PDE_multiple, PDE_list, deltax)
     
     assert all(i == 1 for i in result)
+
+
+## Now we are going to test the  calculate_total_mass(self, PDE_list, SSA_list, PDE_multiple, deltax, SSA_M) function.
+
+
+def test_calculate_total_mass_1(wrapper):
+    """Ensure calculate_total_mass returns two NumPy arrays"""
+    SSA_M = 2
+    PDE_multiple = 2
+    deltax = 0.1
+    PDE_list = [1, 2, 3, 4]
+    SSA_list = [5, 6]
+
+    combined, approx_mass = wrapper.calculate_total_mass(PDE_list, SSA_list, PDE_multiple, deltax, SSA_M)
+
+    assert isinstance(combined, np.ndarray)
+    assert isinstance(approx_mass, np.ndarray)
+
+
+def test_calculate_total_mass_2(wrapper):
+    """Ensure calculate_total_mass returns two NumPy arrays"""
+    SSA_M = 2
+    PDE_multiple = 4
+    deltax = 0.25
+    PDE_list = [1, 2, 3, 4, 5, 6, 7, 8]  #(1+2+3+4)/4=2.5, (5+6+7+8)/4=6.5 Then we add them to 5 and 6.
+    SSA_list = [5, 6]
+
+    combined, approx_mass = wrapper.calculate_total_mass(PDE_list, SSA_list, PDE_multiple, deltax, SSA_M)
+
+    combined_query = np.array([7.5, 12.5], dtype=np.float32) #Should be this asnwer
+    assert np.array_equal(combined, combined_query)
+
+
+
