d684bfd98ff6289aa26b0e3796d3b4bb96a8b121
Cgyc20 <cgyc20@bath.ac.uk>
2025-04-04 10:17:00 +0100

    Add multiple tests for approximate_mass_left_hand in test_c_functions.py

--- tests/test_c_functions.py
+++ tests/test_c_functions.py
@@ -7,13 +7,47 @@ from src.c_class import CFunctionWrapper  # Adjust the import based on your proj
 def wrapper():
     return CFunctionWrapper("src/c_class/C_functions.so")
 
-def test_approximate_mass_left_hand(wrapper):
+def test_approximate_mass_left_hand_1(wrapper):
+    """This will test the approximate left hand of a PDE discretised list. WIll test that the instance is a np.ndarray"""
     SSA_M = 10
-    PDE_multiple = 2
-    PDE_list = np.random.rand(20).astype(np.float32)
+    PDE_multiple = 4
+    PDE_list = np.ones(SSA_M*PDE_multiple).astype(np.float32)
     deltax = 0.1
 
     result = wrapper.approximate_mass_left_hand(SSA_M, PDE_multiple, PDE_list, deltax)
 
     assert isinstance(result, np.ndarray)
+   
+
+def test_approximate_mass_left_hand_2(wrapper):
+    """This will test the approximate left hand of a PDE discretised list"""
+    SSA_M = 10
+    PDE_multiple = 4
+    PDE_list = np.ones(SSA_M*PDE_multiple).astype(np.float32)
+    deltax = 0.1
+
+    result = wrapper.approximate_mass_left_hand(SSA_M, PDE_multiple, PDE_list, deltax)
+
     assert len(result) == SSA_M
+    
+def test_approximate_mass_left_hand_3(wrapper):
+    """This will test the approximate left hand of a PDE discretised list"""
+    SSA_M = 10
+    PDE_multiple = 4
+    PDE_list = np.ones(SSA_M*PDE_multiple).astype(np.float32)
+    deltax = 0.1
+
+    result = wrapper.approximate_mass_left_hand(SSA_M, PDE_multiple, PDE_list, deltax)
+    
+    assert result[0] == 0.4 #This should be 1*0.1+1*0.1+1*0.1+1*0.1
+
+def test_approximate_mass_left_hand_3(wrapper):
+    """This will test the approximate left hand of a PDE discretised list"""
+    SSA_M = 10
+    PDE_multiple = 10
+    PDE_list = np.ones(SSA_M*PDE_multiple).astype(np.float32)
+    deltax = 0.1
+
+    result = wrapper.approximate_mass_left_hand(SSA_M, PDE_multiple, PDE_list, deltax)
+    
+    assert all(i == 1 for i in result)
